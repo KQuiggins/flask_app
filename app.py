6213eb1e3f84387460c6d4ce21083ad0912d32ba
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-05-24 08:26:49 -0400

    added update user profile form to dashboard

--- app.py
+++ app.py
@@ -154,6 +154,24 @@ def login():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+    form = UserForm()
+    id = current_user.id
+    name_to_update = User.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        name_to_update.username = request.form['username']
+        try:
+            db.session.commit()
+            flash('User Updated Successfully')
+            return render_template('dashboard.html', form=form, name_to_update=name_to_update, id=id)
+        except:
+            flash('There was an issue updating the user')
+            return render_template('dashboard.html', form=form, name_to_update=name_to_update, id=id)
+    else:
+        return render_template('dashboard.html', form=form, name_to_update=name_to_update, id=id)
+    
     return render_template('dashboard.html')
 
 
@@ -182,6 +200,7 @@ def posts(id):
 
 
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
 def edit_post(id):
     post = Post.query.get_or_404(id)
     form = PostForm()
@@ -210,6 +229,7 @@ def post():
 
 
 @ app.route('/add-post', methods=['GET', 'POST'])
+@login_required
 def add_post():
     form = PostForm()
     if form.validate_on_submit():
@@ -265,13 +285,14 @@ def update_user(id):
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
         name_to_update.favorite_color = request.form['favorite_color']
+        name_to_update.username = request.form['username']
         try:
             db.session.commit()
             flash('User Updated Successfully')
-            return render_template('update.html', form=form, name_to_update=name_to_update)
+            return render_template('update.html', form=form, name_to_update=name_to_update, id=id)
         except:
             flash('There was an issue updating the user')
-            return render_template('update.html', form=form, name_to_update=name_to_update)
+            return render_template('update.html', form=form, name_to_update=name_to_update, id=id)
     else:
         return render_template('update.html', form=form, name_to_update=name_to_update, id=id)
 
